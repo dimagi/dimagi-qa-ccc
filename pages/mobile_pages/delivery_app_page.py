22277303883810b22ba1b729f241e8dcaf92f627
Apple <apple@Mac.lan>
2025-12-29 10:52:20 +0530

    mobile scripts WIP connect 9, 10

--- pages/mobile_pages/delivery_app_page.py
+++ pages/mobile_pages/delivery_app_page.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.mobile_pages.base_page import BasePage
 from utils.helpers import LocatorLoader
 
@@ -6,3 +8,34 @@ locators = LocatorLoader("locators/mobile_locators.yaml", platform="mobile")
 class DeliveryAppPage(BasePage):
 
     DELIVERY_APP_HEADER_TXT = locators.get("delivery_app_page", "delivery_app_header_txt")
+    DELIVERY_START_BTN = locators.get("delivery_app_page", "delivery_app_start_btn")
+    CASE_LIST_BTN = locators.get("delivery_app_page", "case_list_btn")
+    CASE_FORMS_BTN = locators.get("delivery_app_page", "case_forms_btn")
+    NAME_INPUT = locators.get("delivery_app_page", "name_input")
+    NEXT_BTN = locators.get("delivery_app_page", "next_btn")
+    FINISH_BTN = locators.get("delivery_app_page", "finish_btn")
+    ID_INPUT = locators.get("delivery_app_page", "id_input")
+    RECORD_LOCATION_BTN = locators.get("delivery_app_page", "record_location_btn")
+
+    def submit_form(self, form_name):
+        self.click_element(self.DELIVERY_START_BTN)
+        time.sleep(1)
+        self.click_element(self.CASE_LIST_BTN)
+        for el in self.get_elements(self.CASE_FORMS_BTN):
+            if form_name in el.text.lower():
+                el.click()
+                break
+
+        self.type_element(self.NAME_INPUT, "Automation User")
+        self.click_element(self.NEXT_BTN)
+        self.type_element(self.ID_INPUT, "101")
+        self.click_element(self.NEXT_BTN)
+        self.click_element(self.RECORD_LOCATION_BTN)
+        time.sleep(2)
+        self.wait_for_element(self.FINISH_BTN)
+        self.click_element(self.FINISH_BTN)
+
+    def verify_payment_info(self):
+        pass
+
+
--- tests/mobile_tests/test_tc_5.py
+++ tests/mobile_tests/test_tc_5.py
@@ -1,12 +1,14 @@
 import allure
 import pytest
 
+from pages.mobile_pages.delivery_app_page import DeliveryAppPage
 from pages.mobile_pages.learn_app_page import LearnAppPage
 from pages.mobile_pages.notifications import Notifications
 from pages.mobile_pages.opportunity_page import OpportunityPage
 from pages.mobile_pages.personal_id_page import PersonalIDPage
 from pages.mobile_pages.home_page import HomePage
 from pages.web_pages.cchq_login_web_page import LoginPage
+from pages.web_pages.connect_home_web_page import ConnectHomePage
 from pages.web_pages.connect_opportunities_web_page import ConnectOpportunitiesPage
 from pages.web_pages.connect_opportunity_dashboard_web_page import OpportunityDashboardPage
 from pages.web_pages.connect_workers_web_page import ConnectWorkersPage
@@ -14,21 +16,29 @@ from pages.web_pages.connect_workers_web_page import ConnectWorkersPage
 
 @allure.feature("CONNECT")
 @allure.story("Delivery App related validations")
-@allure.tag("CONNECT_9")
+@allure.tag("CONNECT_9", "CONNECT_10")
 @allure.description("""
   This automated test consolidates multiple manual test cases
 
   Covered manual test cases:
   - CONNECT_9 : Verify user can view the opportunity details and claim the job after completing the learning
+  - CONNECT_10 : Verify on updating a visit status from web, the status of the visit gets updated on the mobile
   """)
+
 @pytest.mark.mobile
-def test_opportunity_details(mobile_driver, test_data):
+@pytest.mark.web
+def test_opportunity_details(web_driver, mobile_driver, config, test_data):
     data = test_data.get("TC_5")
+
+    cchq_login_page = LoginPage(web_driver)
+    cchq_home_page = HomePage(web_driver)
+    connect_home_page = ConnectHomePage(web_driver)
 
     # mobile driver and page initiation
     pid = PersonalIDPage(mobile_driver)
     home = HomePage(mobile_driver)
     learn = LearnAppPage(mobile_driver)
+    delivery = DeliveryAppPage(mobile_driver)
 
 
     with allure.step("Click on Sign In / Register"):
@@ -49,3 +59,15 @@ def test_opportunity_details(mobile_driver, test_data):
 
     with allure.step("Download the Delivery App"):
         learn.download_delivery_app()
+
+    with allure.step("Submit the form on the Delivery App"):
+        delivery.submit_form("Registration Form")
+
+    with allure.step("Verify Payment Unit Info"):
+        delivery.verify_payment_info()
+
+    with allure.step("Login to CommCare HQ and SignIn Connect with CommCare HQ"):
+        cchq_login_page.valid_login_cchq(config)
+        cchq_home_page.verify_home_page_title("Welcome")
+        cchq_login_page.navigate_to_connect_page(config)
+        connect_home_page.signin_to_connect_page_using_cchq()
