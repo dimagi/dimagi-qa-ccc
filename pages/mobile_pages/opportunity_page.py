2d12821930531676b3009462a452f8b7a0aeba76
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2026-04-16 22:59:46 +0530

    updated and added new locators

--- pages/mobile_pages/opportunity_page.py
+++ pages/mobile_pages/opportunity_page.py
@@ -16,6 +16,7 @@ class OpportunityPage(BasePage):
     JOB_COMPLETE_BY_DATE_TXT = locators.get("opportunity_page", "job_complete_by_date_text")
 
     DELIVERY_DETAILS_REVIEW_TXT = locators.get("opportunity_page", "delivery_details_review_txt")
+    DELIVERY_TITLE_TXT = locators.get("opportunity_page", "delivery_title_txt")
     TOTAL_VISIT_TXT = locators.get("opportunity_page", "total_visit_txt")
     DELIVERY_DAYS_TXT = locators.get("opportunity_page", "delivery_days_txt")
     DELIVERY_MAX_DAILY_TXT = locators.get("opportunity_page", "delivery_max_daily_txt")
@@ -24,6 +25,7 @@ class OpportunityPage(BasePage):
 
     LEARN_DETAILS_TXT = locators.get("opportunity_page", "learn_details_txt")
     LEARN_TITLE_TXT = locators.get("opportunity_page", "learn_title_txt")
+    LEARN_COMPLETE_TXT = locators.get("opportunity_page", "learn_complete_txt")
     INTRO_LEARNING_MODULE_TXT = locators.get("opportunity_page", "intro_learn_module_txt")
     INTRO_LEARN_SUMMARY_TXT = locators.get("opportunity_page", "intro_learn_summary_txt")
     DOWNLOAD_LEARN_APP_BTN = locators.get("opportunity_page", "download_learn_app_btn")
@@ -74,7 +76,7 @@ class OpportunityPage(BasePage):
     def verify_delivery_details(self):
         self.click_element(self.VIEW_MORE_BTN)
         menu_items = [
-            self.DELIVERY_DETAILS_REVIEW_TXT,
+            self.DELIVERY_TITLE_TXT,
             self.TOTAL_VISIT_TXT,
             self.DELIVERY_DAYS_TXT,
             self.DELIVERY_MAX_DAILY_TXT,
@@ -84,19 +86,20 @@ class OpportunityPage(BasePage):
         for item in menu_items:
             assert self.is_displayed(item), f"Delivery details not visible: {item}"
 
-        self.click_element(self.CLOSE_DELIVERY_DETAILS_BTN)
+        # self.click_element(self.CLOSE_DELIVERY_DETAILS_BTN)
 
     def verify_learn_details(self):
         menu_items = [
             self.LEARN_DETAILS_TXT,
-            self.LEARN_TITLE_TXT,
+            self.LEARN_COMPLETE_TXT,
             self.INTRO_LEARN_SUMMARY_TXT,
-            self.INTRO_LEARNING_MODULE_TXT,
-            self.DOWNLOAD_LEARN_APP_BTN
+            # self.INTRO_LEARNING_MODULE_TXT,
+            # self.DOWNLOAD_LEARN_APP_BTN
         ]
 
         for item in menu_items:
             assert self.is_displayed(item), f"Learn details not visible: {item}"
+            print(f"Learn details visible: {item}")
 
     def verify_opportunity_list(self):
         time.sleep(2)
